f38c6413f5fa0c8c7506ec21a812880cc373c0be
NguyenThiMai2k4 <125457090+NguyenThiMai2k4@users.noreply.github.com>
2025-07-28 19:51:38 +0700

    ignore application

---
+++ DocuLearn/src/main/java/com/ntm/doculearn/pojo/Student1.java
@@ -0,0 +1,28 @@
+package com.ntm.doculearn.pojo;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "student")
+public class Student1 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Integer id;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "student_code", nullable = false, length = 50)
+    private String studentCode;
+
+}
---
+++ DocuLearn/src/main/java/com/ntm/doculearn/pojo/SummaryOption.java
@@ -0,0 +1,4 @@
+package com.ntm.doculearn.pojo;
+
+public class SummaryOption {
+}
